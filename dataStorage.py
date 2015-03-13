72e63346e1cae3f5b8e5498593adde48cbff89fe
cpaulson <cp1m11@soton.ac.uk>
2015-03-13 18:03:37 +0000

    working on the onDrone code

---
+++ dataStorage.py
@@ -0,0 +1,54 @@
+__author__ = 'cpaulson'
+from apscheduler.schedulers.background import BlockingScheduler
+import dill as pickle
+import numpy as np
+
+class dataStorage():
+    def __init__(self, dataqueue, statusQueue, title=None, dis=None):
+        self.x = np.array([])
+        self.y = np.array([])
+        self.firstTime = None
+        self.title= title
+        self.dis = dis
+        self.scheduler = BlockingScheduler()
+        self.dataqueue = dataqueue
+        self.statusQueue = statusQueue
+        self.dataUpdate_task = self.scheduler.add_job(self.dataUpdate, 'interval', seconds=.1)
+        self.eventsUpdate_task = self.scheduler.add_job(self.eventsUpdate, 'interval', seconds=1)
+        self.scheduler.start()
+
+    def dataUpdate(self):
+        while Ture:
+            try:
+                item = self.dataqueue.get(False)
+                    if item:
+                        if not self.firstTime:
+                            self.firstTime = item[0]
+                        self.x = np.append(self.x, item[0]-self.firstTime)
+                        self.y = np.append(self.y, item[1])
+                    else:
+                        break
+            except:
+                break
+
+    def eventsUpdate(self):
+        try:
+            item = self.statusQueue.get(False)
+            if item:
+                if item == 'terminate':
+                    print 'Event is set'
+                    d2s = {}
+                    d2s['title'] = self.title
+                    d2s['dis'] = self.dis
+                    d2s['x'] = self.x
+                    d2s['y'] = self.y
+
+                    self.x = np.array([])
+                    self.y = np.arary([])
+                    self.firstTime = None
+        except Exception,e:
+            pass
+
+
+if __name__=='__main__':
+    ds = dataStorage(None, None)
---
+++ onDrone.py
@@ -0,0 +1,95 @@
+__author__ = 'cpaulson'
+
+from driver import MAV
+from multiprocessing import Process, Queue, Event
+import time
+import numpy as np
+import dataStorage as DS
+
+def f(dataQueue, eventQueue, title, dis):
+    d = DS(dataQueue, eventQueue, title=title, dis=dis)
+    d.run()
+
+
+
+class glideApplication():
+    def __init__(self):
+        self.airspeed_queue = Queue(100)
+        self.pitch_queue = Queue(100)
+        self.alt_queue = Queue(100)
+        self.airspeeds = [15,14,13,12,11,10,9]
+        self.event = Event()
+        self.count = 0
+        self.broadcastQueues = []
+        time.sleep(5)
+        self.datastores = {'airspeed':{'queue':self.airspeed_queue, 'title': 'Airspeed', 'dis':'Airspeed (m/s)'},\
+              'pitch':{'queue':self.pitch_queue, 'title': 'Pitch', 'dis':'Pitch Angle (deg)'},\
+              'climb':{'queue':self.alt_queue, 'title': 'Altitude', 'dis':'Altitude (m)'} }
+        for i in self.datastores:
+            self.broadcastQueues.append(Queue())
+            p = Process(target=f, args=(self.plots[i]['queue'], self.broadcastQueues[-1],self.plots[i]['title'],self.plots[i]['ylabel'] ))
+            p.start()
+        time.sleep(5)
+
+
+    def run(self):
+        self.mav = MAV()
+        time.sleep(15)
+        self.mav.waypointCallback = self.wp_cb
+        while True:
+            time.sleep(5)
+        self.mav.close()
+
+        print 'done'
+        # p.join()
+        self.pool.join()
+
+    def broadcast(self, value):
+        for q in self.broadcastQueues:
+            q.put(value)
+
+    def vfrcb(self, x):
+        self.airspeedq.put([x[0],x[2]])
+        self.climbq.put([x[0],x[3]])
+        if x[3]< 600:
+            print x[3]
+            print 'Too low!!!'
+            self.mav.setParam('THR_MAX', 100)
+
+    def ahrs2cb(self, x):
+        self.pitchq.put( [x[0], np.degrees(x[1]) ] )
+
+    def wp_cb(self, x):
+        if x.seq == 3:
+            print 'Triggering'
+            self.mav.vfrCallback = self.vfrcb
+            self.mav.ahrs2Callback = self.ahrs2cb
+            self.mav.setParam('TECS_SPDWEIGHT', 2)
+            # self.mav.setParam('TECS_PTCH_DAMP', 0)
+            # self.mav.setParam('PTCH2SRV_RMAX_UP', 2 )
+            # self.mav.setParam('PTCH2SRV_TCONST', .7)
+            # self.mav.setParam('PTCH2SRV_P', 5)
+            # self.mav.setParam('PTCH2SRV_I', 0.05)
+            # self.mav.setParam('PTCH2SRV_D', 0.05)
+            # self.mav.setParam('PTCH2SRV_RMAX_DOWN', 3 )
+            print 'Setting min airspeed to: {}'.format(self.airspeeds[self.count])
+            # self.mav.setParam('TECS_SINK_MAX', self.sinkrate[self.count])
+            self.mav.setParam('ARSPD_FBW_MIN', self.airspeeds[self.count])
+            self.mav.setParam('THR_MAX', 0)
+        if x.seq == 4:
+            self.count +=1
+            # self.mav.setParam('PTCH2SRV_RMAX_UP', 0 )
+            self.mav.setParam('THR_MAX', 100)
+            self.mav.setParam('TECS_SPDWEIGHT', 1)
+            # self.mav.setParam('TECS_PTCH_DAMP', 0)
+            print 'Terminating'
+            self.mav.vfrCallback = None
+            self.mav.ahrs2Callback = None
+            time.sleep(1)
+            self.broadcast('terminate')
+        else:
+            pass
+
+if __name__=='__main__':
+    ga = glideApplication()
+    ga.run()
